9c2f38f18d65b75b3db95fb1f1eb646681a38f84
matthewtols <44300541+matthewtols@users.noreply.github.com>
2020-04-22 11:09:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+var thefile;
 
-  var openFile = function(event) {
-    var input = event.target;
+var openFile = function(event) {
+  var input = event.target;
 
-    var reader = new FileReader();
-    reader.onload = function(){
-      var text = reader.result;
-      let theFile = (reader.result.substring(0, 1000000000));
-    };
-    reader.readAsText(input.files[0]);
+  var reader = new FileReader();
+  reader.onload = function(){
+    var text = reader.result;
+    theFile = (reader.result.substring(0, 1000000000));
   };
+  reader.readAsText(input.files[0]);
+};
 
 document.getElementById("submit").addEventListener("click", logit)
 function logit() {
