e54a946e76fdfe18c0413eda1b81d1aa25c6c045
matthewtols <44300541+matthewtols@users.noreply.github.com>
2020-04-22 17:09:08 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var openFile = function(event) {
   var reader = new FileReader();
   reader.onload = function(){
     var text = reader.result;
-    theFile = (reader.result.substring(75000, 90000));
+    theFile = (reader.result.substring(15000, 16000));
   };
   reader.readAsText(input.files[0]);
 };
