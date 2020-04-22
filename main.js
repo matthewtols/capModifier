124462828ba2f4fe65f9a04631d2ef9aaf0fa0e1
matthewtols <44300541+matthewtols@users.noreply.github.com>
2020-04-22 09:53:11 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-
-var filesjson = document.getElementById("myFile").value
-console.log(filesjson)
+document.getElementById("submit").addEventListener("click", load)
+function load() {
+  var filesjson = document.getElementById("myFile").value
+  console.log(filesjson)
+}
