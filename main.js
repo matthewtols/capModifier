4d1d186f9a9f8a72ea51673d33cd15010bb64248
matthewtols <44300541+matthewtols@users.noreply.github.com>
2020-04-22 10:36:48 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-document.getElementById("submit").addEventListener("click", load)
-function load() {
-  var filesjson = document.getElementById("myFile").value
-  console.log(filesjson)
-}
+
+  var openFile = function(event) {
+    var input = event.target;
+
+    var reader = new FileReader();
+    reader.onload = function(){
+      var text = reader.result;
+      console.log(reader.result.substring(0, 1000000000));
+    };
+    reader.readAsText(input.files[0]);
+  };
